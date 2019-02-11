5d379db86fad44fd8490a3e8ff86fd14e8e3257f
gutosie <gutosie@o2.pl>
2019-02-11 20:53:42 +0200

    neoboot2

--- iNB.sh
+++ iNB.sh
@@ -6,7 +6,7 @@ if `grep -q 'osd.language=pl_PL' </etc/enigma2/settings`; then
   PL=1
 fi
 [ -e /tmp/neoboot.zip ] && rm -f /tmp/neoboot.zip
-[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+[ -e /tmp/neoboot2-master ] && rm -rf /tmp/neoboot2-master
 [ $PL ] && echo "Pobieranie archiwum..." || echo "Downloading archive file..."
 URL='https://github.com/gutosie/neoboot2./archive/master.zip'
 curl -kLs $URL  -o /tmp/neoboot.zip
@@ -21,10 +21,11 @@ rm -f /tmp/neoboot..zip
 [ $PL ] && echo "Instalowanie..." || echo "Instaling..."
 Cel="/usr/lib/enigma2/python/Plugins/Extensions"
 [ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
-mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
-[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+mv -f /tmp/neoboot2-master/NeoBoot/* $Cel/NeoBoot
+[ -e /tmp/neoboot2-master ] && rm -rf /tmp/neoboot2-master
 cd $Cel/NeoBoot
 chmod 755 ./bin/*
+chmod 755 ./target/*
 chmod 755 ./ex_init.py
 chmod 755 ./files/*.sh
 chmod -R +x ./ubi_reader/*
