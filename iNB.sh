612c698d620f8dd370c72c996586ea496f4a0dbd
gutosie <gutosie@o2.pl>
2019-02-11 21:05:11 +0200

    neoboot 2

--- iNB.sh
+++ iNB.sh
@@ -8,15 +8,15 @@ fi
 [ -e /tmp/neoboot.zip ] && rm -f /tmp/neoboot.zip
 [ -e /tmp/neoboot2-master ] && rm -rf /tmp/neoboot2-master
 [ $PL ] && echo "Pobieranie archiwum..." || echo "Downloading archive file..."
-URL='https://github.com/gutosie/neoboot2./archive/master.zip'
+URL='https://github.com/gutosie/neoboot2/archive/master.zip'
 curl -kLs $URL  -o /tmp/neoboot.zip
 cd /tmp/
 if [ ! -e /tmp/master.zip ]; then 
    wget $URL  
    mv -f /tmp/master.zip /tmp/neoboot.zip  
 fi
-unzip -qn ./neoboot..zip
-rm -f /tmp/neoboot..zip
+unzip -qn ./neoboot.zip
+rm -f /tmp/neoboot.zip
 #kopiowanie
 [ $PL ] && echo "Instalowanie..." || echo "Instaling..."
 Cel="/usr/lib/enigma2/python/Plugins/Extensions"
@@ -25,7 +25,6 @@ mv -f /tmp/neoboot2-master/NeoBoot/* $Cel/NeoBoot
 [ -e /tmp/neoboot2-master ] && rm -rf /tmp/neoboot2-master
 cd $Cel/NeoBoot
 chmod 755 ./bin/*
-chmod 755 ./target/*
 chmod 755 ./ex_init.py
 chmod 755 ./files/*.sh
 chmod -R +x ./ubi_reader/*
